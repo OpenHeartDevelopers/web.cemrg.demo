828e4f6424eba05275dbce973b0a5d33275ffd40
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2024-02-23 17:13:48 +0000

    add border and styling of bottons and font

--- mini-game/src/MainPage.js
+++ mini-game/src/MainPage.js
@@ -6,18 +6,43 @@ import { Link } from 'react-router-dom';
 const MainPage = () => {
   const buttonStyle = {
     padding: '10px 20px',
-    fontSize: '16px',
+    fontSize: '56px',
     margin: '5px',
-    backgroundColor: '#3498db', // Blue color as an example
+    backgroundColor: '#833471', // Blue color as an example
     color: '#ffffff', // White text
     border: 'none',
     borderRadius: '5px',
     cursor: 'pointer',
+    textDecoration: 'none', // Remove underline from links
+  };
+
+  const videoContainerStyle = {
+    textAlign: 'center',
+    margin: '20px 0',
+  };
+
+  const contentContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh', // Adjust the height as needed
   };
 
   return (
-    <div>
-      <h1>Calibration Game - Can you match this heart?</h1>
+    <div style={contentContainerStyle}>
+      {/* Title */}
+      <h1 style={{ textAlign: 'center', fontSize: '66px', fontFamily: 'Arial' }}>Can you match this heart?</h1>
+
+      {/* Video Container */}
+      <div style={videoContainerStyle}>
+        <video width="600" height="400" controls autoPlay loop>
+          <source src="motion_pvloop.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
+      {/* Menu Buttons */}
       <div id="menu">
         <Link to="/level1" style={buttonStyle}>Level 1</Link>
         <Link to="/level2" style={buttonStyle}>Level 2</Link>
